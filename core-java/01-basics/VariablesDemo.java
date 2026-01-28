565108adefd21052f8f10a3a8f2fec02ba472872
NEHA1039 <nehathorat2004@gmail.com>
2026-01-28 22:10:55 +0530

    Day 1 Variables and DataTypes in java

---
+++ core-java/01-basics/VariablesDemo.java
@@ -0,0 +1,29 @@
+/**
+ * Topic: Variables and Data Types in Java
+ *
+ * Concepts Covered:
+ * - Primitive data types
+ * - Non-primitive data type (String)
+ * - Basic output
+ *
+ * Author: Neha Thorat
+ * Day: 1
+ */
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+        double cgpa = 8.8;
+        char grade = 'A';
+        boolean isLearningJava = true;
+
+        String name = "Neha";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Learning Java: " + isLearningJava);
+    }
+}
